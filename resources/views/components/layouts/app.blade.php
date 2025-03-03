61e172bc6e1eaab70c4b46e31b631308e28a17a4
gracieuxsikuly <graciersikuly@gmail.com>
2025-03-03 11:06:53 +0200

    crud

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,5 +1,12 @@
+{{-- menu horozontal --}}
 <x-layouts.app.sidebar>
     <flux:main>
         {{ $slot }}
     </flux:main>
 </x-layouts.app.sidebar>
+{{-- menu verticale a haut --}}
+{{-- <x-layouts.app.header>
+    <flux:main container>
+        {{ $slot }}
+    </flux:main>
+</x-layouts.app.header> --}}
